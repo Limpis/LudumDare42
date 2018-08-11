60c42ae4eea9182dabe69567ee28d56bde15e81d
Limpis <linus.andersson91@outlook.com>
2018-08-11 22:40:10 +0200

    Added scripts for randomizing items with itemsellers

---
+++ LudumDare42/Assets/Script/ItemSeller.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSeller : MonoBehaviour {
+
+    public List<GameObject> possibleItems = new List<GameObject>();
+    public List<Button> sellerItemButtons = new List<Button>();
+
+    private void Start()
+    {
+        for (int i = 0; i < sellerItemButtons.Count; i++)
+        {
+            int r = Random.Range(0, possibleItems.Count);
+
+            ItemSlot itemSlot;
+            itemSlot = sellerItemButtons[i].GetComponent<ItemSlot>();
+            itemSlot.PlaceItem(possibleItems[r]);
+            possibleItems.RemoveAt(r);
+        }
+    }
+}
